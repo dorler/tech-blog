9b66b241469e835f6d0321b4645be27e112fa09b
Deb Orler <debbieorler@gmail.com>
2021-07-15 20:11:41 -0500

    server setup

---
+++ models/Comment.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    body: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Comments;
